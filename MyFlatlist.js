4f720f4bbc92cf52c7ffef84d96619947052b05a
SyedaHumnaAshfaq <shumnaashfaq@gmail.com>
2023-03-10 02:19:38 +0500

    second commit

--- MyFlatlist.js
+++ MyFlatlist.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, Alert,Image} from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Alert,Image, TextInput} from 'react-native';
 import { mainViewstyle,view1style,view2style,view3style,view4style,view5style,view6style} from './style';
 export default function MyFlatlist() {
   const [myarray,setmyarray] = useState([
@@ -74,38 +74,60 @@ export default function MyFlatlist() {
   
 return(
     <View style={mainViewstyle.container}>
-        <View style={view4style.view4}>
-          <Text style={{textAlign:'center',paddingTop:30,fontSize:30,color:'orange'}}>
-          Foodie Moodie</Text>
+        
+        <View style={{flex:0.2,paddingTop:70}}>
+          <Text style={{fontSize:20,marginLeft:5,fontWeight:'bold',paddingBottom:15}}>Choose your favourite food</Text>
+          <View  style={{flexDirection:'row'}}>
+          <Image source={require('./assets/searchIcon.png')} style={{height:40,width:35,resizeMode:'stretch',borderBottomLeftRadius:15,borderTopLeftRadius:15,marginLeft:5,backgroundColor:'white'}}/>
+          <TextInput style={{backgroundColor:'white',height:40,width:270,borderBottomRightRadius:15,borderTopRightRadius:15,fontSize:15}}>Search your favourite food</TextInput>
+         
+         </View>
+          
         </View>
-        <Text style={{fontSize:20}}>Select Category: </Text>
-        <Text>     </Text>
+        <View style={{flex:0.4,marginTop:10,marginBottom:0,paddingTop:10,backgroundColor:'lightgrey'}}>
+        <View style={{flexDirection:'row',marginBottom:20}}>
+        <Text style={{fontSize:20,paddingTop:10,fontWeight:'bold',marginLeft:5}}>Food Category</Text>
+        <TouchableOpacity style={{marginLeft:200,marginTop:15}}>
+        <Text>See All</Text>
+        </TouchableOpacity>
+        </View>
+        
         <FlatList
         horizontal
           data={myarray}
           keyExtractor={item=>item.key}
           renderItem={({item})=>(
         
-        <View style={{flex:0.1,backgroundColor:'orange'}}>
-          <View style={{flex:0.1,marginHorizontal:5,backgroundColor:'black',alignItems:'center'}}>
-          <Text style={{paddingTop:10,color:'orange'}}>     {item.title}      </Text>
+        // <View style={{flex:0.02,backgroundColor:'blue'}}>
+          <View style={{flex:0.02,height:50,width:100,marginHorizontal:5,marginTop:0,backgroundColor:'white',borderRadius:11,alignItems:'center',textAlign:'center'}}>
+          <TouchableOpacity>
+          <Text style={{paddingTop:10,color:'black',fontWeight:'bold'}}>{item.title}</Text>
+          </TouchableOpacity>
+          
+          
+          </View>)}/>
           </View>
-        </View>)}/>
-
-
+        {/* </View> */}
+        <View style={{flex:1.2,backgroundColor:'lightgrey'}}>
+        <Text style={{fontSize:20,marginLeft:5,fontWeight:'bold'}}>Popular Food</Text>
+        <Text>   </Text>
 
         <FlatList
         horizontal
         data={myimagearray}
         keyExtractor={item=>item.key}
         renderItem={({item})=>(
-          <View style={{flex:0.5,backgroundColor:'orange'}}>
-        <View style={{flex:0.5,marginHorizontal:7,backgroundColor:'black',alignItems:'center',width:200}}>
-        <Text style={{paddingTop:10,color:'orange',fontSize:20}}>     {item.title}      </Text>
-        <Image source={item.photo} style={{height:140,width:190}}></Image>
+          // <View style={{flex:0.5,backgroundColor:'lightgrey',marginTop:0}}>
+           
         
+        <View style={{flex:0.5,marginHorizontal:7,height:150,backgroundColor:'white',alignItems:'center',width:200,borderRadius:11}}>
+        <TouchableOpacity style={{textAlign:'center',alignItems:'center'}}> 
+        <Text style={{paddingTop:5,color:'black',fontSize:20}}>     {item.title}      </Text>
+        <Image source={item.photo} style={{height:110,width:190,borderRadius:11}}></Image>
+        </TouchableOpacity> 
+        </View>)}/>
         </View>
-          </View>)}/>
+          {/* </View> */}
 
        
         
--- style.js
+++ style.js
@@ -5,13 +5,13 @@ const mainViewstyle = StyleSheet.create({
     container:{
         flex:1,
     marginTop:0,
-    backgroundColor:'orange',
+    backgroundColor:'lightgrey',
     alignContent:'center'}
     })
     const view1style = StyleSheet.create({
         view1:{
             flex:0.4,
-            backgroundColor:"orange",
+            backgroundColor:"lightgrey",
             alignItems:'center',
             alignContent:'center',
             paddingTop:50
@@ -20,7 +20,7 @@ const mainViewstyle = StyleSheet.create({
     const view2style = StyleSheet.create({
         view2:{
             flex:0.5,
-            backgroundColor:'orange',
+            backgroundColor:'lightgrey',
             //alignContent:'center',
             alignItems:'center',
             paddingTop:40
@@ -30,7 +30,7 @@ const mainViewstyle = StyleSheet.create({
     const view3style = StyleSheet.create({
         view3:{
             flex:0.6,
-            backgroundColor:'orange',
+            backgroundColor:'lightgrey',
             alignItems:'center',
             marginTop:0,
             //marginBottom:20,
@@ -42,7 +42,7 @@ const mainViewstyle = StyleSheet.create({
     const view4style =StyleSheet.create({
         view4:{
             flex:0.2,
-            backgroundColor:'black',
+            backgroundColor:'lightgrey',
             alignContent:'center',
             alignItems:'center',
 
@@ -52,7 +52,7 @@ const mainViewstyle = StyleSheet.create({
     const view5style =StyleSheet.create({
         view5:{
             flex:0.4,
-            backgroundColor:'orange',
+            backgroundColor:'lightgrey',
             alignItems:'center',
             alignContent:'center'
 
@@ -62,7 +62,7 @@ const mainViewstyle = StyleSheet.create({
     const view6style =StyleSheet.create({
         view6:{
             flex:0.1,
-            backgroundColor:'black'
+            backgroundColor:'lightgrey'
 
         }
         
